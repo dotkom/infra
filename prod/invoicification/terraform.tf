04256cc92a4a766aff0a28b3e0fff8709cb94efa
Mats Jun <mats@jun.codes>
2024-03-13 18:16:34 +0100

    Deploy invoice form to Vercel (dotkom/terraform-monorepo#120)

---
+++ prod/invoicification/terraform.tf
@@ -0,0 +1,45 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-monorepo.online.ntnu.no"
+    key    = "invoicification-prod.tfstate"
+    region = "eu-north-1"
+  }
+
+  required_version = "~> 1.7.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33"
+    }
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 1.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+
+  default_tags {
+    tags = {
+      Project = "invoicification-prod"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project = "invoicification-prod"
+    }
+  }
+}
+
+provider "vercel" {
+  team = "dotkom"
+}
