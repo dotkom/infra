26ef921a8965c4f44ab4f3e90fb5014bec5a4133
Mats Larsen <mats@jun.codes>
2024-10-05 18:34:43 +0900

    Deploy Monoweb RPC to staging

---
+++ staging/monoweb-rpc/ecs.tf
@@ -0,0 +1,32 @@
+module "rpc_evergreen_service" {
+  source = "../../modules/evergreen-service"
+
+  service_name = "monoweb-staging-rpc"
+
+  acm_certificate_arns = [module.rpc_domain_certificate.certificate_arn]
+  domain_names         = [local.rpc_domain_name]
+
+  target_group_container_name = "monoweb-staging-rpc"
+  target_group_container_port = 4444
+  target_group_rule_priority  = 1550
+
+  task_count    = 1
+  task_cpu      = 1024 / 8
+  task_memory   = 1024 / 8
+  task_role_arn = aws_iam_role.rpc.arn
+
+  containers = [
+    {
+      container_name = "monoweb-staging-rpc"
+      image          = data.aws_ecr_image.rpc.image_uri
+      cpu            = 1024 / 8
+      memory         = 1024 / 8
+      essential      = true
+      environment    = data.doppler_secrets.monoweb_rpc.map
+      ports          = [{ container_port = 4444, protocol = "tcp" }]
+      healthcheck = {
+        command = ["CMD-SHELL", "curl -f http://0.0.0.0:4444/health 2>/dev/null || exit 1"]
+      }
+    }
+  ]
+}
