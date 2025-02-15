80f3a93d439511745cfe67714b91be429da80d37
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2025-02-15 14:54:42 +0100

    Add SRV record

--- minecraft/route53.tf
+++ minecraft/route53.tf
@@ -5,3 +5,12 @@ resource "aws_route53_record" "minecraft_server" {
   ttl     = 300
   records = ["129.241.153.251"]
 }
+
+resource "aws_route53_record" "minecraft_srv" {
+  zone_id = data.aws_route53_zone.online.zone_id
+  name    = "_minecraft._tcp.play.online.ntnu.no"
+  type    = "SRV"
+  ttl     = 300
+  records = ["0 5 25565 play.online.ntnu.no"]
+}
+
