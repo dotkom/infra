494a0b7bf24f3c62269b34e573b9767cb7968305
Mats Jun Larsen <mats@jun.codes>
2025-02-27 18:23:16 +0100

    Fix missed formatting changes

--- prod/monoweb-web/s3.tf
+++ prod/monoweb-web/s3.tf
@@ -3,10 +3,10 @@ locals {
 }
 
 module "static_bucket" {
-  source               = "../../modules/aws-s3-public-bucket"
-  certificate_arn      = module.cdn_domain_certificate.certificate_arn
-  domain_name          = local.cdn_domain_name
-  zone_id              = data.aws_route53_zone.online_ntnu_no.zone_id
+  source          = "../../modules/aws-s3-public-bucket"
+  certificate_arn = module.cdn_domain_certificate.certificate_arn
+  domain_name     = local.cdn_domain_name
+  zone_id         = data.aws_route53_zone.online_ntnu_no.zone_id
   cors_allowed_origins = [
     "http://localhost:3002"
   ]
