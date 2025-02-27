5cbd212d491316ef02d9d91ebca46310fbbb5a46
Mats Jun Larsen <mats@jun.codes>
2025-02-27 18:46:40 +0100

    Configure GitHub Actions role for Grades Web in production (dotkom/terraform-monorepo#165)

---
+++ prod/grades/github.tf
@@ -0,0 +1,48 @@
+module "server_ci" {
+  source = "../../modules/github-actions-iam"
+
+  role_name = "GradesProdServerCIRole"
+  repository_scope = [
+    "repo:dotkom/gradestats:*",
+  ]
+}
+
+module "web_ci" {
+  source = "../../modules/github-actions-iam"
+
+  role_name = "GradesProdWebCIRole"
+  repository_scope = [
+    "repo:dotkom/gradestats-app:*",
+  ]
+}
+
+data "aws_iam_policy_document" "web_ci_role" {
+  statement {
+    actions   = ["ecr:GetAuthorizationToken"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+    effect = "Allow"
+    resources = [
+      module.web_ecr_image.ecr_repository_arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "web_ci_policy" {
+  name   = "GradesProdWebCIPolicy"
+  policy = data.aws_iam_policy_document.web_ci_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "web_ci_role" {
+  policy_arn = aws_iam_policy.web_ci_policy.arn
+  role       = module.web_ci.role.name
+}
