83765c93ed9baacdc43870fc28a929950787ae8b
Mats Jun Larsen <mats@jun.codes>
2025-03-13 16:50:55 +0100

    Add GitHub Actions role for dashboard staging deployment (dotkom/terraform-monorepo#183)

---
+++ staging/monoweb-dashboard/github.tf
@@ -0,0 +1,40 @@
+module "dashboard_ci" {
+  source = "../../modules/github-actions-iam"
+
+  role_name = "MonowebProdDashboardCIRole"
+  repository_scope = [
+    "repo:dotkom/monoweb:*"
+  ]
+}
+
+data "aws_iam_policy_document" "dashboard_ci_role" {
+  statement {
+    actions   = ["ecr:GetAuthorizationToken"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+    effect = "Allow"
+    resources = [
+      module.server_ecr_image.ecr_repository_arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "dashboard_ci_role" {
+  name   = "MonowebProdDashboardCIPolicy"
+  policy = data.aws_iam_policy_document.dashboard_ci_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "dashboard_ci_role" {
+  policy_arn = aws_iam_policy.dashboard_ci_role.arn
+  role       = module.dashboard_ci.role.name
+}
+
