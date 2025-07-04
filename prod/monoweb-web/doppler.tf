3b3ed1fa2ebcd29582ff46709ad612c235c0a0e9
Mats Jun Larsen <mats@jun.codes>
2025-07-05 00:39:28 +0900

    Deduplicate SENTRY_DSN environment variables (dotkom/terraform-monorepo#218)

--- prod/monoweb-web/doppler.tf
+++ prod/monoweb-web/doppler.tf
@@ -10,11 +10,3 @@ resource "doppler_secret" "sentry_dsn" {
   name  = "SENTRY_DSN"
   value = sentry_key.monoweb_web.dsn.public
 }
-
-resource "doppler_secret" "next_public_sentry_dsn" {
-  project = data.doppler_secrets.monoweb_web.project
-  config  = data.doppler_secrets.monoweb_web.config
-
-  name  = "NEXT_PUBLIC_SENTRY_DSN"
-  value = sentry_key.monoweb_web.dsn.public
-}
--- staging/monoweb-dashboard/doppler.tf
+++ staging/monoweb-dashboard/doppler.tf
@@ -10,11 +10,3 @@ resource "doppler_secret" "sentry_dsn" {
   name  = "SENTRY_DSN"
   value = sentry_key.monoweb_dashboard.dsn.public
 }
-
-resource "doppler_secret" "next_public_sentry_dsn" {
-  project = data.doppler_secrets.monoweb_dashboard.project
-  config  = data.doppler_secrets.monoweb_dashboard.config
-
-  name  = "NEXT_PUBLIC_SENTRY_DSN"
-  value = sentry_key.monoweb_dashboard.dsn.public
-}
