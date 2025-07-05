4f73b9760d2be724a168e0b46586e0952696b80c
Mats Jun Larsen <mats@jun.codes>
2025-07-05 16:47:25 +0900

    Remove remaining Gatus infrastructure

--- prod/gatus/node/release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 891459268445.dkr.ecr.eu-north-1.amazonaws.com
-docker build -t 891459268445.dkr.ecr.eu-north-1.amazonaws.com/monoweb/prd/gatus:latest --platform linux/amd64 -f Dockerfile .
-docker push 891459268445.dkr.ecr.eu-north-1.amazonaws.com/monoweb/prd/gatus:latest
