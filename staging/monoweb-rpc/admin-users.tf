28a52a4f22e5b663494122105d433cf3b43517a8
Mats Jun Larsen <mats@jun.codes>
2025-08-23 18:20:03 +0200

    Remove legacy ADMIN_USERS variable (dotkom/terraform-monorepo#229)

--- staging/monoweb-rpc/admin-users.tf
+++
@@ -1,7 +0,0 @@
-resource "doppler_secret" "admin_users" {
-  config  = data.doppler_secrets.monoweb_rpc.config
-  project = data.doppler_secrets.monoweb_rpc.project
-
-  name  = "ADMIN_USERS"
-  value = "*"
-}
