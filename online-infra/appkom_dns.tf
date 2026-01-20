9ef0d3ca6159ef438961f7fb8df65e71743102cd
johngothe <117284605+johngothe@users.noreply.github.com>
2026-01-20 21:05:17 +0100

    Add CNAME record for autobank subdomain (#32)

--- online-infra/appkom_dns.tf
+++ online-infra/appkom_dns.tf
@@ -13,3 +13,11 @@ resource "aws_route53_record" "appkom_jubileum_dns" {
   ttl     = 3600
   records = ["cname.vercel-dns.com"]
 }
+
+resource "aws_route53_record" "appkom_autobank_dns" {
+  name    = "autobank.online.ntnu.no"
+  type    = "CNAME"
+  zone_id = aws_route53_zone.online_ntnu_no.id
+  ttl     = 3600
+  records = ["cb59bebf2653ef11.vercel-dns-016.com."]
+}
