ca66a2c540882baa4f9a68672ff66f53261eaf12
Mats Jun Larsen <mats@jun.codes>
2026-02-04 18:54:04 +0000

    Eliminate arm64 part of ECS cluster (#49)

--- prod/evergreen/ecs.tf
+++ prod/evergreen/ecs.tf
@@ -22,26 +22,12 @@ resource "aws_ecs_capacity_provider" "amd64" {
   }
 }
 
-resource "aws_ecs_capacity_provider" "arm64" {
-  name = "evergreen-prod-ec2-nodes-arm64"
-
-  auto_scaling_group_provider {
-    auto_scaling_group_arn = aws_autoscaling_group.evergreen_arm64_scaling_group.arn
-    managed_scaling {
-      maximum_scaling_step_size = 1
-      minimum_scaling_step_size = 1
-      status                    = "ENABLED"
-      target_capacity           = 80
-    }
-  }
-}
-
 resource "aws_ecs_cluster_capacity_providers" "evergreen" {
   cluster_name       = aws_ecs_cluster.evergreen.name
-  capacity_providers = [aws_ecs_capacity_provider.amd64.name, aws_ecs_capacity_provider.arm64.name, "FARGATE"]
+  capacity_providers = [aws_ecs_capacity_provider.amd64.name, "FARGATE"]
 
   default_capacity_provider_strategy {
-    capacity_provider = aws_ecs_capacity_provider.amd64.name
+    capacity_provider = "FARGATE"
     weight            = 100
     base              = 0
   }
