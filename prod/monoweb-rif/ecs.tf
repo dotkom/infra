64cab6175dc5051eff56a176c7eab25b86b0f018
Mats Jun Larsen <mats@jun.codes>
2026-02-04 18:51:48 +0000

    Migrate rif to Fargate (#46)

--- prod/monoweb-rif/ecs.tf
+++ prod/monoweb-rif/ecs.tf
@@ -1,3 +1,25 @@
+data "aws_vpc" "evergreen" {
+  filter {
+    name   = "tag:Name"
+    values = ["evergreen-prod-vpc"]
+  }
+}
+
+data "aws_subnets" "vpc_private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.evergreen.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["evergreen-prod-private-*"]
+  }
+}
+
+data "aws_security_group" "evergreen_node" {
+  name = "evergreen-prod-node"
+}
+
 module "rif_evergreen_service" {
   source = "../../modules/evergreen-service"
 
@@ -11,19 +33,22 @@ module "rif_evergreen_service" {
   target_group_rule_priority  = 1100
 
   task_count    = 1
-  task_cpu      = 1024 / 8
-  task_memory   = 1024 / 8
+  task_cpu      = 1024 / 4
+  task_memory   = 1024 / 2
   task_role_arn = aws_iam_role.ecs_task.arn
 
   runtime_platform_architecture     = "ARM64"
   runtime_platform_operating_system = "LINUX"
 
+  vpc_subnets        = data.aws_subnets.vpc_private.ids
+  vpc_security_group = data.aws_security_group.evergreen_node.id
+
   containers = [
     {
       container_name = "monoweb-prd-rif"
       image          = data.aws_ecr_image.rif.image_uri
-      cpu            = 1024 / 8
-      memory         = 1024 / 8
+      cpu            = 1024 / 4
+      memory         = 1024 / 2
       essential      = true
       environment    = data.doppler_secrets.rif.map
       ports          = [{ container_port = 3000, protocol = "tcp" }]
