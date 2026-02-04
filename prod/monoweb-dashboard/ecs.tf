ab5e8eaeeb59dbe948f4c26a23e42dac8621f302
Mats Jun Larsen <mats@jun.codes>
2026-02-04 18:51:07 +0000

    Migrate dashboard to Fargate (#44)

--- prod/monoweb-dashboard/ecs.tf
+++ prod/monoweb-dashboard/ecs.tf
@@ -1,3 +1,25 @@
+data "aws_vpc" "evergreen" {
+  filter {
+    name   = "tag:Name"
+    values = ["evergreen-prod-vpc"]
+  }
+}
+
+data "aws_subnets" "vpc_private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.evergreen.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["evergreen-prod-private-*"]
+  }
+}
+
+data "aws_security_group" "evergreen_node" {
+  name = "evergreen-prod-node"
+}
+
 module "dashboard_evergreen_service" {
   source = "../../modules/evergreen-service"
 
@@ -11,18 +33,21 @@ module "dashboard_evergreen_service" {
   target_group_rule_priority  = 1601
 
   task_count    = 1
-  task_cpu      = 1024 / 2
+  task_cpu      = 1024 / 4
   task_memory   = 1024 / 2
   task_role_arn = aws_iam_role.dashboard.arn
 
   runtime_platform_architecture     = "ARM64"
   runtime_platform_operating_system = "LINUX"
 
+  vpc_subnets        = data.aws_subnets.vpc_private.ids
+  vpc_security_group = data.aws_security_group.evergreen_node.id
+
   containers = [
     {
       container_name = "monoweb-prd-dashboard"
       image          = data.aws_ecr_image.dashboard.image_uri
-      cpu            = 1024 / 2
+      cpu            = 1024 / 4
       memory         = 1024 / 2
       essential      = true
       environment    = data.doppler_secrets.monoweb_dashboard.map
