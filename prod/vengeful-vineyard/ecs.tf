3c7a95bdb4e9e5ca91fcd61cba3fca09dc0995dc
Sondre Alfnes <sondre.alfnes@gmail.com>
2026-02-24 12:09:40 +0100

    Change to arm64

--- prod/vengeful-vineyard/ecs.tf
+++ prod/vengeful-vineyard/ecs.tf
@@ -37,7 +37,7 @@ module "server_evergreen_service" {
   task_memory   = 1024 / 2
   task_role_arn = aws_iam_role.server.arn
 
-  runtime_platform_architecture     = "X86_64"
+  runtime_platform_architecture     = "ARM64"
   runtime_platform_operating_system = "LINUX"
 
   vpc_subnets        = data.aws_subnets.vpc_private.ids
